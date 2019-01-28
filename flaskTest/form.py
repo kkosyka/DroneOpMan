eb1b9b225f3331c735e82f1758a239463c164b5d
Kalynn Kosyka <kkosyka@gmail.com>
2019-01-27 19:53:53 -0500

    flask stuff

--- flaskTest/form.py
+++ flaskTest/form.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, json, request
+import flask
 # from flaskext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
 import pymysql
@@ -37,6 +38,14 @@ class Database:
         self.con.close
         return result
 
+    def get_pilot(self, name):
+        self.cur = self.con.cursor()
+        self.cur.execute("SELECT * FROM DroneOp.crew WHERE pilot = '" + name +"'");
+        result = self.cur.fetchall()
+        self.cur.close
+        self.con.close
+        return result
+
     def list_equipment(self):
         self.cur.execute("SELECT * FROM DroneOp.equipment LIMIT 50")
         result = self.cur.fetchall()
@@ -67,6 +76,27 @@ def form():
     res.insert(0,temp)
     return render_template('form.html', res=res)
 
+# @app.route('/update')
+# def update():
+#     return render_template('update.html', content_type='application/json')
+#
+# @app.route('/calculate_result')
+# def calculate_result():
+#     a = int(request.args.get('val1'))
+#     b = int(request.args.get('val2'))
+#     return flask.jsonify({"result":a+b})
+
+@app.route('/update_pilot_info')
+def update_pilot_info():
+    selectedPilot = request.args.get('selectedPilot')
+    # def db_query():
+    #     db = Database()
+    #     pilotInfo = db.get_pilot()
+    #     return pilotInfo
+    # res = db_query()
+    # return res
+    return flask.jsonify({"result":selectedPilot})
+
 @app.route('/list_crew', methods=['POST','GET'])
 def crew():
     def db_query():
@@ -78,25 +108,27 @@ def crew():
     return render_template('list_crew.html', result=res, content_type='application/json')
 
 
-@app.route('/list_equipment', methods=['POST','GET'])
-def equip():
-    def db_query():
-        db = Database()
-        # equipment = db.list_equipment()
-        self.cur.execute("SELECT * FROM DroneOp.equipment LIMIT 50")
-        equipment = self.cur.fetchall()
-        return equipment
-    res = db_query()
-    return render_template('list_equipment.html', result=res, content_type='application/json')
 
-@app.route('/list_equip_details', methods=['POST','GET'])
-def equip_details():
-    def db_query():
-        db = Database()
-        equip_details = db.list_equip_details()
-        return equip_details
-    res = db_query()
-    return render_template('list_equip_details.html', result=res, content_type='application/json')
+#
+# @app.route('/list_equipment', methods=['POST','GET'])
+# def equip():
+#     def db_query():
+#         db = Database()
+#         # equipment = db.list_equipment()
+#         self.cur.execute("SELECT * FROM DroneOp.equipment LIMIT 50")
+#         equipment = self.cur.fetchall()
+#         return equipment
+#     res = db_query()
+#     return render_template('list_equipment.html', result=res, content_type='application/json')
+#
+# @app.route('/list_equip_details', methods=['POST','GET'])
+# def equip_details():
+#     def db_query():
+#         db = Database()
+#         equip_details = db.list_equip_details()
+#         return equip_details
+#     res = db_query()
+#     return render_template('list_equip_details.html', result=res, content_type='application/json')
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
