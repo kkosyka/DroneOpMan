406dd6415988bc64500e8d4530be3805b08bbc5e
Kalynn Kosyka <kkosyka@gmail.com>
2019-01-29 13:04:34 -0500

    weather stuff?

--- flaskTest/form.py
+++ flaskTest/form.py
@@ -3,6 +3,9 @@ import flask
 # from flaskext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
 import pymysql
+from datetime import date
+import datetime
+# from weather import Weather, Unit
 
 app = Flask(__name__)
 
@@ -22,12 +25,8 @@ class Database:
         self.cur.execute("SELECT pilot FROM DroneOp.crew")
         row_headers=[x[0] for x in self.cur.description] #this will extract row headers
         results = self.cur.fetchall()
-        # json_data=[]
-        # for result in results:
-        #      json_data.append(dict(zip(row_headers,result)))
         self.cur.close
         self.con.close
-        # return json.dumps(json_data)
         return results
 
     def list_crew(self):
@@ -47,18 +46,50 @@ class Database:
         return result
 
     def list_equipment(self):
-        self.cur.execute("SELECT * FROM DroneOp.equipment LIMIT 50")
+        self.cur.execute("SELECT * FROM DroneOp.equipment")
+        result = self.cur.fetchall()
+        self.cur.close
+        self.con.close
+        return result
+
+    def list_models_equipment(self):
+        self.cur.execute("SELECT aircraft_mod FROM DroneOp.equipment")
+        row_headers=[x[0] for x in self.cur.description] #this will extract row headers
+        result = self.cur.fetchall()
+        self.cur.close
+        self.con.close
+        return result
+
+    def get_model(self, name):
+        self.cur = self.con.cursor()
+        self.cur.execute("SELECT * FROM DroneOp.equipment WHERE aircraft_mod = '" + name +"'");
         result = self.cur.fetchall()
         self.cur.close
         self.con.close
         return result
 
     def list_equip_details(self):
-        self.cur.execute("SELECT * FROM DroneOp.equip_details LIMIT 50")
+        self.cur.execute("SELECT * FROM DroneOp.equip_details")
         result = self.cur.fetchall()
         self.cur.close
         self.con.close
         return result
+
+# def weather():
+#     weather = Weather(unit=Unit.FAHRENHEIT)
+#     # location = weather.lookup_by_location('northampton, ma')
+#     # condition = location.condition
+#     # temp = location.condition
+#     # print(condition.text)
+#     # print(condition.temp)
+#
+#     lookup = weather.lookup_by_latlng(42.318274,-72.637246)
+#     condition = lookup.condition
+#     temp = location.condition
+#     # print(condition.text)
+#     # print(condition.temp)
+#     return condition.text, condition.temp
+#
 
 @app.route('/')
 def init():
@@ -70,11 +101,15 @@ def form():
     def db_query():
         db = Database()
         crew = db.list_crew_names()
-        return crew
-    res = db_query()
-    temp = "Pilot's Name"
-    res.insert(0,temp)
-    return render_template('form.html', res=res)
+        model = db.list_models_equipment()
+        return crew, model
+    crewRes, modelRes = db_query()
+    tempName = "Pilot's Name"
+    tempModel = "Models"
+    crewRes.insert(0,tempName)
+    modelRes.insert(0, tempModel)
+    # conditionText, temp = weather()
+    return render_template('form.html', crewRes=crewRes, modelRes=modelRes)
 
 # @app.route('/update')
 # def update():
@@ -89,13 +124,42 @@ def form():
 @app.route('/update_pilot_info')
 def update_pilot_info():
     selectedPilot = request.args.get('selectedPilot')
-    # def db_query():
-    #     db = Database()
-    #     pilotInfo = db.get_pilot()
-    #     return pilotInfo
-    # res = db_query()
-    # return res
-    return flask.jsonify({"result":selectedPilot})
+    def db_query():
+        db = Database()
+        pilotInfo = db.get_pilot(selectedPilot)
+        return pilotInfo
+    r = json.dumps(db_query())
+    res = json.loads(r)[0]
+    return flask.jsonify({"PicTitle":str(res['pic_title']),
+                            "FaaUasCertNum":str(res['faa_uas_cert_num']),
+                            "IssueOn":str(res['issue_on']),
+                            "ValidUntil":str(res['valid_until']),
+                            "AmaNum":str(res['ama_num'])})
+
+@app.route('/update_model_info')
+def update_model_info():
+    selectedModel = request.args.get('selectedModel')
+    def db_query():
+        db = Database()
+        modelInfo = db.get_model(selectedModel)
+        return modelInfo
+    r = json.dumps(db_query())
+    res = json.loads(r)[0]
+    list = str(res['drones']).split(",")
+    return flask.jsonify({"result":list})
+
+
+
+# @app.route('/update_pilot_info')
+# def update_pilot_info():
+#     selectedPilot = request.args.get('selectedPilot')
+#     # def db_query():
+#     #     db = Database()
+#     #     pilotInfo = db.get_pilot()
+#     #     return pilotInfo
+#     # res = db_query()
+#     # return res
+#     return flask.jsonify({"result":selectedPilot})
 
 @app.route('/list_crew', methods=['POST','GET'])
 def crew():
